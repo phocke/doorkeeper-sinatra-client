6775e3f02e7e51412309d185fb794359babd9aec
Przemek Hocke <phocke@gmail.com>
2012-11-10 11:11:35 +0100

    settings for heroku

---
+++ env.rb
@@ -0,0 +1,4 @@
+ENV['OAUTH2_CLIENT_ID']           = "9407ec5615c7bd51a19bd39d63d41afdc0b5d7b0a3a5b5048454ddd085075540"
+ENV['OAUTH2_CLIENT_SECRET']       = "1d740f721f1966f92c345244e29dc5172cbd65307de7c5d86388222081eac200"
+ENV['OAUTH2_CLIENT_REDIRECT_URI'] = "http://ideonetsamplesinatra.herokuapp.com/callback"
+ENV['SITE']                       = "http://ideonetapp.herokuapp.com"
